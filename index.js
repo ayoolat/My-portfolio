204c72e357113329575f929ece9f9692269b4a13
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2020-10-01 20:40:33 +0100

    Added responsiveness

--- index.js
+++ index.js
@@ -9,15 +9,23 @@ const navbar = document.querySelector(".navbar");
 const ham = document.querySelector(".ham");
 const hamCon = document.querySelector("ham-con");
 const hamElement1 = document.querySelector(".ham-element1");
+const hamElement2 = document.querySelector(".ham-element2");
 const hamElement = document.querySelector(".ham-element");
 const navCollapse = document.querySelector("nav-Collapse");
 
 ham.addEventListener("click", toggleHamburger);
 
 function toggleHamburger(){
-    navbar.classList.toggle("showNav");
-    hamElement.classList.toggle("last");
-    hamElement1.classList.toggle("first");
+    hamElement.classList.toggle("open");
+    hamElement1.classList.toggle("open");
+    hamElement2.classList.toggle("open");
 }
 
-
+function showFullNav(){
+  let fullPageNav = document.getElementById("hamburger-nav")
+  if (fullPageNav.style.display == "block"){
+    fullPageNav.style.display = "none"
+  }else{
+    fullPageNav.style.display = "block"
+  }
+}
